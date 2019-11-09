f7e8d377ccda33b01a23ff406ccf94b6b00ec15d
vimal <vimalsingh99.2@gmail.com>
2019-11-09 18:28:50 +0530

    Add starter code

---
+++ src/main/java/com/example/consumingrestservice/ConsumingRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumingrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestServiceApplication.class, args);
+	}
+
+}
